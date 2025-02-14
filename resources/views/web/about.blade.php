ea0a72d1e7a50e2c2740fddaed739a1b497c1782
arsalanahmed <arsalanahmeddev1@gmail.com>
2025-02-14 05:15:40 +0500

    initial commit

---
+++ resources/views/web/about.blade.php
@@ -0,0 +1,35 @@
+@extends('web/layouts.app')
+@section('title', 'About')
+
+@section('content')
+    <section class="hero-banner">
+        <div class="container">
+            <div class="flex justify-between pb-[243px]">
+                <div>
+                    <h1 class="text-[30px] font-medium text-white uppercase mb-[46px]">Start Your Career in <br> Dental Assisting
+                        Today!</h1>
+                    <div class="flex items-center gap-[10px]">
+                        <a href="" class="btn-primary btn block max-w-[164px] text-center">Enroll Now</a>
+                        <a href="" class="btn-primary btn btn-blur !text-white !text-[18px] !font-medium">Contact Us</a>
+                    </div>
+                </div>
+                <div>
+                    <p class="max-w-[425px] text-right mb-[40px]">
+                        Lorem ipsum dolor sit amet, consectetur elit. Ut elit tellus, luctus nec ullamcorper mattis ipsum dolor sit amet, consectetur elit.
+                    </p>
+                    <a href="javascript:;" class="btn-transparent btn flex justify-center ml-auto max-w-[206px] text-[18px]">Class Schedule</a>
+                </div>
+                <div>
+                    <h2 class="text-[80px] text-white italic font-bold max-w-[871px] leading-[90px]">
+                        Learn from Industry Experts!
+                    </h2>
+                </div>
+                <div class="flex ">
+                    <span class="text-white text-[50px] ">10k+</span>
+                    <div class="h-[100px] "></div>
+                </div>
+            </div>
+           
+        </div>
+    </section>
+@endsection
---
+++ resources/views/web/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header class="header absolute top-0 w-full py-[20px]">
+    <div class="container mx-auto">
+        <div class="flex justify-between items-center">
+            <div class="logo">
+                <img src="{{ asset('web/images/logo.png') }}" alt="logo">
+            </div>
+            <nav class="">
+                <ul class="primary-nav flex gap-[40px]">
+                    <li><a href="javascript:;">About</a></li>
+                    <li><a href="javascript:;">Dental Assistant</a></li>
+                    <li><a href="javascript:;">EFDA</a></li>
+                    <li><a href="javascript:;">Coronal Polishing & Fluoride Application</a></li>
+                    <li><a href="javascript:;">Payment Options</a></li>
+                    <li><a href="javascript:;">Gallery</a></li>
+                </ul>
+            </nav>
+            <div class="flex justify-between items-center gap-[34px]">
+                <a href="tel:317.515.5996" class="text-white font-bold italic text-[20px]">317.515.5996</a>
+                <a href="javascript:;" class="btn-primary btn">Student Portal</a>
+            </div>
+        </div>
+    </div>
+</header>
